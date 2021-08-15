920aa4c2967e5fb1a23f6323ee58114de174bcd0
tarunkumarnahak25 <tarunnaha25@gmail.com>
2021-08-15 14:26:04 +0530

    added technologies section

--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -1,12 +1,64 @@
-import React from 'react';
-import { DiFirebase, DiReact, DiZend } from 'react-icons/di';
-import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles/GlobalComponents';
-import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './TechnologiesStyles';
+import React from "react";
+import { DiCoda, DiFirebase, DiPython, DiReact, DiZend } from "react-icons/di";
+import {
+  Section,
+  SectionDivider,
+  SectionText,
+  SectionTitle,
+} from "../../styles/GlobalComponents";
+import {
+  List,
+  ListContainer,
+  ListItem,
+  ListParagraph,
+  ListTitle,
+} from "./TechnologiesStyles";
 
-const Technologies = () =>  (
-  <div>
-    Technologies
-  </div>
+const Technologies = () => (
+  <Section id="tech">
+    <SectionDivider />
+    <br />
+    <br />
+
+    <SectionTitle>Technologies</SectionTitle>
+
+    <SectionText>
+      I've worked with various Technologies with including from frontend
+      frameworks to backend and data science .
+    </SectionText>
+    <List>
+      <ListItem>
+        <DiReact size="3rem"></DiReact>
+        <ListContainer>
+          <ListTitle>Front-End</ListTitle>
+          <ListParagraph>
+            Experience with <br />
+            React.js
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <DiFirebase size="3rem"></DiFirebase>
+        <ListContainer>
+          <ListTitle>Back-End</ListTitle>
+          <ListParagraph>
+            Experience with <br />
+            Node, Django and Database
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <DiPython size="3rem"></DiPython>
+        <ListContainer>
+          <ListTitle>Data Science</ListTitle>
+          <ListParagraph>
+            Experience with <br />
+            Python, Tensorflow
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+    </List>
+  </Section>
 );
 
 export default Technologies;
