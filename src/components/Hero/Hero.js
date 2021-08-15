01560a92c909887ba606f102217ce466e607c961
tarunkumarnahak25 <tarunnaha25@gmail.com>
2021-08-15 13:29:43 +0530

    added hero section

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,13 +1,29 @@
-import React from 'react';
+import React from "react";
 
-import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
-import Button from '../../styles/GlobalComponents/Button';
-import { LeftSection } from './HeroStyles';
+import {
+  Section,
+  SectionText,
+  SectionTitle,
+} from "../../styles/GlobalComponents";
+import Button from "../../styles/GlobalComponents/Button";
+import { LeftSection } from "./HeroStyles";
 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Welcome to <br />
+        My Personal Tarun's folio
+      </SectionTitle>
+      <SectionText>
+        I'm Tarun a Great Learner and good programmer. I'm a data scientst and a
+        fullstack computer science engineer.
+      </SectionText>
+      <Button onClick={() => (window.location = "https://google.com")}>
+        Learn More
+      </Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
